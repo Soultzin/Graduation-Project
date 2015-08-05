8508fe6eb3dee6c17637c272be857f34d0cce0f7
Soultzin <mackaygarrett115@gmail.com>
2015-08-05 17:30:49 -0400

    Jquery step in the right direction

---
+++ profilefolder/profileapp.js
@@ -0,0 +1,8 @@
+
+
+
+$(document).ready(function(){
+    $("#button").click(function(){
+        $("#GoalLabel").append("New Goal: <input type='text'><br><button class='btn btn-primary' id>Create</button>");
+    });
+});
