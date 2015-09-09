60cc7edc44ccf582d3a3b806b0b1a3632f189a71
Michael Merriman <TheMikeMerriman@gmail.com>
2015-09-09 18:19:38 -0400

    Add Goal Attempt

--- profilefolder/profileapp.js
+++ profilefolder/profileapp.js
@@ -1,11 +1,7 @@
 $(document).ready(function(){
-    $("#button").click(function(){
-        $("#CGoal").detatch;
-    });
+
     $("#button").click(function(){
         $("#GoalLabel").append("<div id = 'CGoal'> New Goal: <input type='text'><br><button id = 'Create' class='btn btn-primary'>Create</button> </div>");
-    });
-    $("#Create").click(function(){
-        $("#CGoal").detatch;
+            $("#button").prop("disabled",true);
     });
 });
