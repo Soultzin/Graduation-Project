9f0544b20c0cb6545d9327aade619986cfbeb020
Michael Merriman <TheMikeMerriman@gmail.com>
2015-09-09 17:04:02 -0400

    Cleaning up Gary's horrible coding

--- profilefolder/profileapp.js
+++ profilefolder/profileapp.js
@@ -1,6 +1,3 @@
-
-
-
 $(document).ready(function(){
     $("#button").click(function(){
         $("#GoalLabel").append("New Goal: <input type='text'><br><button class='btn btn-primary' id>Create</button>");
