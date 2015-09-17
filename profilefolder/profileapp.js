e0f3067307e4d8acb2cb3312a7c155ec1dd8f217
tomrose <tomhrose@gmail.com>
2015-09-17 17:45:55 -0400

    Use Case: Add Goal
    updated code to use a dialog box to prompt the user to enter in a goal

--- profilefolder/profileapp.js
+++ profilefolder/profileapp.js
@@ -4,14 +4,14 @@ $(document).ready(function(){
 });
 function createGoal()
 {
-    $("#NewGoal").append("<div id = 'CGoal'> New Goal: <input type='text'><br><button id = 'Create' class='btn btn-primary' onclick='saveGoal();'>Create</button> </div>");
-    $('#butGoal').removeAttr('onClick');
+
+    $('#createGoalDialog').modal();
 
 }
 
 function saveGoal()
 {
     alert("saveGoal");
-    $('#butGoal').attr('onclick','createGoal()');
-    $('#NewGoal').remove();
+    $('#createGoalDialog').modal('hide');
+
 }
