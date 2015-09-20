578297ab5130a63e622a9b731ca288477652076e
Garrett Mackay <mackaygarrett115@gmail.com>
2015-09-19 23:35:20 -0400

    Attempt to Make Goal Creations Work
    In this change, I tried to make the input values for the "Create Goals"
    actually append a new table for a new goal and subgoals. The problem I
    ran into though is this: when I make jquery that states input.value =
    new goaldiv.value, the divs for the "New Goals" have to have different
    names each time a new goal needs to be made. I will have to consult Tom.

--- profilefolder/profileapp.js
+++ profilefolder/profileapp.js
@@ -11,8 +11,11 @@ function createGoal()
 
 function saveGoal()
 {
-    alert("Your Goal was saved");
+    alert('Your Goal was saved');
     $('#createGoalDialog').modal('hide');
+    $('#GoalLabel').append(" <div id='Goal2'><table width='66%' border='1px'><thead><tr><th colspan='3' id='G1'><center><img id= 'removeButton' src='http://i795.photobucket.com/albums/yy232/DaedalusIcarusHelios/closeX.png' onclick='removeGoal();'>Get Straight A's</center></th></tr></thead><tbody><tr><td>Step 1:</td><td div='sub2.1'>Do All Homework</td><td name='due2.1'>DUE BLEHEHEH</td></tr><tr><td>Step 2:</td><td name='sub2.2>Dracula</td><td name='due2.2'>DUE BLEHEHEH</td></tr><tr><td>Step 3:</td><td name='sub2.3'>Bride of Frankenstein</td><td name='due2.3'>DUE BLEHEHEH</td></tr><tr><td>Step 4:</td><td name='sub2.4'>Bride of Frankenstein</td><td name='due2.4'>DUE BLEHEHEH</td></tr></tbody></table><br><div class='progress'><div class='progress-bar'role='progressbar' aria-valuenow='100' aria-valuemin='0'aria-valuemax='100' style='width: 100%;' id='thebar'>100%</div></div>");
+    
+    
 
 }
 function removeGoal()
@@ -20,9 +23,3 @@ function removeGoal()
     $('#Goal1').remove();
 }
 
-function FillBilling(f) {
-  if(f.billingtoo.checked == true) {
-    f.billingname.value = f.shippingname.value;
-    f.billingcity.value = f.shippingcity.value;
-  }
-}
