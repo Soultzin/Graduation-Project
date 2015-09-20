f918846d5afd1dc19606227e002af837327ad6a9
Garrett Mackay <mackaygarrett115@gmail.com>
2015-09-19 21:34:33 -0400

    Link from Login to Profile
    problem: only works for my file system

--- profilefolder/profileapp.js
+++ profilefolder/profileapp.js
@@ -19,3 +19,10 @@ function removeGoal()
 {
     $('#Goal1').remove();
 }
+
+function FillBilling(f) {
+  if(f.billingtoo.checked == true) {
+    f.billingname.value = f.shippingname.value;
+    f.billingcity.value = f.shippingcity.value;
+  }
+}
