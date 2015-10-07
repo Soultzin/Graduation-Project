013570393b3ea2cc51c8e34e8cdced7223ea0c6e
tomrose <tomhrose@gmail.com>
2015-10-06 21:01:21 -0400

    updated for adding goals and steps

--- profilefolder/profileapp.js
+++ profilefolder/profileapp.js
@@ -4,18 +4,43 @@ $(document).ready(function(){
 });
 function createGoal()
 {
-
     $('#createGoalDialog').modal();
-
 }
 
 function saveGoal()
 {
-    alert('Your Goal was saved');
     $('#createGoalDialog').modal('hide');
-    $('#GoalLabel').append(" <div id='Goal2'><table width='66%' border='1px'><thead><tr><th colspan='3' id='G1'><center><img id= 'removeButton' src='http://i795.photobucket.com/albums/yy232/DaedalusIcarusHelios/closeX.png' onclick='removeGoal();'>Get Straight A's</center></th></tr></thead><tbody><tr><td>Step 1:</td><td div='sub2.1'>Do All Homework</td><td name='due2.1'>DUE BLEHEHEH</td></tr><tr><td>Step 2:</td><td name='sub2.2>Dracula</td><td name='due2.2'>DUE BLEHEHEH</td></tr><tr><td>Step 3:</td><td name='sub2.3'>Bride of Frankenstein</td><td name='due2.3'>DUE BLEHEHEH</td></tr><tr><td>Step 4:</td><td name='sub2.4'>Bride of Frankenstein</td><td name='due2.4'>DUE BLEHEHEH</td></tr></tbody></table><br><div class='progress'><div class='progress-bar'role='progressbar' aria-valuenow='100' aria-valuemin='0'aria-valuemax='100' style='width: 100%;' id='thebar'>100%</div></div>");
-    
-    
+
+    //Let's create the table
+    var goalName = $('#G1Entry').val();
+
+    $('#G1Entry').val("");
+
+    var tableKey = goalName.replace(/[^a-z0-9]|\s+|\r?\n|\r/gmi, "_");
+
+    $('#studentGoals').append("<h1>" + goalName + "</h1><button type='button' class='btn btn-primary' onclick='showGoalStep(\"" + tableKey + "\")'" + ">Add</button>");
+
+    var tableDiv = $("<div class='table-responsive'>");
+
+    $('#studentGoals').append(tableDiv);
+
+    var table = $("<table id='" + tableKey + "' " + "class='table table-striped table-bordered table-hover table-condensed small'>");
+
+    tableDiv.append(table);
+
+    ///
+    /// Table head
+    ///
+    html = "<thead>" +
+        "    <tr>" +
+        "        <th><strong>Description</strong></th>" +
+        "        <th><strong>Value</strong></th>" +
+        "        <th></th>" +
+        "     </tr>" +
+        "</thead>";
+
+    var tableHead = $(html);
+    table.append(tableHead);
 
 }
 function removeGoal()
@@ -25,4 +50,34 @@ function removeGoal()
 
 function removeSub(){
   
+}
+
+function showGoalStep(goalId) {
+
+    console.log("Table Key: " + goalId);
+    $('#stepTableName').val(goalId);
+    $('#createStepDialog').modal();
+
+    console.log("I'm done with the dialog");
+
+}
+
+function createStep() {
+
+    console.log("Step Name: " + $('#stepEntry').val());
+    console.log("Step Date: " + $('#stepDate').val());
+    console.log("goalId: " + $('#stepTableName').val());
+
+    var tableName = $('#stepTableName').val();
+
+    var table = document.getElementById(tableName);
+    var row = table.insertRow();
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+
+    cell1.innerHTML = $('#stepEntry').val();
+    cell2.innerHTML = $('#stepDate').val();
+    cell3.innerHTML = "<button type='button' class='btn btn-default' data-dismiss='modal'>Delete</button>"
+
 }
